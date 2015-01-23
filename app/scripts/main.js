72f1236e8dd26feff89c3f01450dde263d9dfed3
Dylan Perales <dperales@gmail.com>
2015-01-23 02:12:34 -0500

    Finished AIX and Linux errors and corrected searchbox

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,18 @@
 'use strict';
 $(document).ready(function() {
+  $('#aixButton').addClass('active');
   var errnoFirebase = new Firebase('https://errno.firebaseio.com/');
   errnoFirebase.once('value', function(errnoListSnapshot) {
     errnoListSnapshot.forEach(function(erroSnapshot) {
       var errnoNumber = erroSnapshot.child('Number').val();
       var errnoMessage = erroSnapshot.child('Message').val();
       var errnoDesc = erroSnapshot.child('Description').val();
-      $('.errnoTable').append('<tr class=errno><td>' + errnoNumber + '</td><td>' + errnoMessage + '</td><td>' + errnoDesc + '</td>');
-      console.log(errnoNumber + errnoMessage + errnoDesc);
+      $('.errnoTable').append('<tr class="errnoRow"><td>' + errnoNumber + '</td><td>' + errnoMessage + '</td><td>' + errnoDesc + '</td></tr>');
     });
   });
 
   // Attach an asynchronous callback to read the data at our posts reference
-  $('#errnoInput').keyup(function () {
+  $('#errnoInput').keyup(function() {
         var filterArray = [];
         var filter = this.value.toLowerCase();  // no need to call jQuery here
 
@@ -20,7 +20,7 @@ $(document).ready(function() {
 
         var arrayLength = filterArray.length; // Get the length of the filter array
 
-        $('.errno').each(function() {
+        $('.errnoRow').each(function() {
             /* cache a reference to the current .media (you're using it twice) */
             var _this = $(this);
             var title = _this.find('td').text().toLowerCase();
@@ -43,6 +43,46 @@ $(document).ready(function() {
             if (hidden === 0)  {
                _this.show();
             }
+            //$('errno').find( filter ).addClass('success');
         });
     });
+
+    // Select Firebase dependent on selected OS
+    $('#aixButton').click(function () {
+      if (!$('#aixButton').hasClass('active'))
+        {
+          $('#aixButton').addClass('active');
+          $('#linuxButton').removeClass('active');
+          $('.errnoRow').remove();
+          errnoFirebase = new Firebase('https://errno.firebaseio.com/');
+          errnoFirebase.once('value', function(errnoListSnapshot) {
+            errnoListSnapshot.forEach(function(erroSnapshot) {
+              var errnoNumber = erroSnapshot.child('Number').val();
+              var errnoMessage = erroSnapshot.child('Message').val();
+              var errnoDesc = erroSnapshot.child('Description').val();
+              $('.errnoTable').append('<tr class="errnoRow"><td>' + errnoNumber + '</td><td>' + errnoMessage + '</td><td>' + errnoDesc + '</td></tr>');
+            });
+          });
+          document.getElementById("errnoInput").value= "";
+        }
+    });
+
+    $('#linuxButton').click(function () {
+      if (!$('#linuxButton').hasClass('active'))
+        {
+          $('#linuxButton').addClass('active');
+          $('#aixButton').removeClass('active');
+          $('.errnoRow').remove();
+          errnoFirebase = new Firebase('https://errno-linux.firebaseio.com/');
+          errnoFirebase.once('value', function(errnoListSnapshot) {
+            errnoListSnapshot.forEach(function(erroSnapshot) {
+              var errnoNumber = erroSnapshot.child('Number').val();
+              var errnoMessage = erroSnapshot.child('Message').val();
+              var errnoDesc = erroSnapshot.child('Description').val();
+              $('.errnoTable').append('<tr class="errnoRow"><td>' + errnoNumber + '</td><td>' + errnoMessage + '</td><td>' + errnoDesc + '</td></tr>');
+            });
+          });
+          document.getElementById("errnoInput").value= "";
+        }
+      });
 });
